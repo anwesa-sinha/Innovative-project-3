194bb0520c1ce3791f98b5895caba8e70ee2f2a6
Draksha <draksha138@gmail.com>
2023-11-17 14:43:42 +0530

    collecting 26 signs

--- ML/collect_img.py
+++ ML/collect_img.py
@@ -5,25 +5,24 @@ DATA_DIR = './data'
 if not os.path.exists(DATA_DIR):
     os.makedirs(DATA_DIR)
 
-number_of_classes = 3
-dataset_size = 100
+number_of_signs = 26
+dataset_size = 50
 
 cap = cv2.VideoCapture(0)
-for j in range(number_of_classes):
+for j in range(number_of_signs):
     if not os.path.exists(os.path.join(DATA_DIR, str(j))):
         os.makedirs(os.path.join(DATA_DIR, str(j)))
-    print('Collecting data for class {}'.format(j))
+    print('Collecting data for class {}'.format(chr(ord('A') + j )))
 
     done = False
     while True:
         ret, frame = cap.read()
-        cv2.putText(frame, 'Ready? Press "Q" ! :)', (100, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 255, 0), 3,cv2.LINE_AA)
+        cv2.putText(frame, 'If Ready Press "q" ', (100, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 255, 0), 3,cv2.LINE_AA)
         cv2.imshow('frame', frame)
         if cv2.waitKey(25) == ord('q'):
             break
 
     counter = 0
-
     while counter < dataset_size:
         ret, frame = cap.read()
         cv2.imshow('frame', frame)
