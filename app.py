6a445d9421f1afa454ab6ce935882927d3326896
anwesa-sinha <sinhaanwesa@gmail.com>
2023-11-26 21:45:48 +0530

    flask app running but ml problem

--- app.py
+++ app.py
@@ -1,80 +1,46 @@
-from flask import Flask, render_template, request
-from PIL import Image
-import numpy as np
-import io
+from flask import Flask, render_template, Response, jsonify, request
 import cv2
-import mediapipe as mp
+import numpy as np
 import pickle
 
 app = Flask(__name__)
 
-model_dict = pickle.load(open('./ML/model.pickle', 'rb'))
+model_dict = pickle.load(open('ML/model.pickle', 'rb'))
 model = model_dict['model']
 
-mp_hands = mp.solutions.hands
-mp_drawing = mp.solutions.drawing_utils
-mp_drawing_styles = mp.solutions.drawing_styles
-
-hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
-
-labels_dict = {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E', 5: 'F', 6: 'G', 7: 'H',
-               8: 'I', 9: 'J', 10: 'K', 11: 'L', 12: 'M', 13: 'N', 14: 'O', 15: 'P',
-               16: 'Q', 17: 'R', 18: 'S', 19: 'T', 20: 'U', 21: 'V', 22: 'W', 23: 'X', 24: 'Y', 25: 'Z', }
-
-
-def process_gesture(image):
-    data_aux = []
-    x_ = []
-    y_ = []
-
-    H, W, _ = image.shape
-    results = hands.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-
-    if results.multi_hand_landmarks:
-        for hand_landmarks in results.multi_hand_landmarks:
-            for i in range(len(hand_landmarks.landmark)):
-                x = hand_landmarks.landmark[i].x
-                y = hand_landmarks.landmark[i].y
-                data_aux.append(x)
-                data_aux.append(y)
-                x_.append(x)
-                y_.append(y)
-
-        x1 = int(min(x_) * W) - 10
-        y1 = int(min(y_) * H) - 10
-        x2 = int(max(x_) * W) - 10
-        y2 = int(max(y_) * H) - 10
-
-        prediction = model.predict([np.asarray(data_aux)])
-
-        predicted_character = labels_dict[int(prediction[0])]
-
-        cv2.rectangle(image, (x1, y1), (x2, y2), (0, 0, 0), 4)
-        cv2.putText(image, predicted_character, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 0, 0), 3, cv2.LINE_AA)
-
-    return predicted_character
-
+cap = cv2.VideoCapture(0)
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
+def generate_frames():
+    while True:
+        success, frame = cap.read()
+        if not success:
+            break
+        else:
+            # Process the frame and send it to the client
+            _, buffer = cv2.imencode('.jpg', frame)
+            frame_data = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame_data + b'\r\n')
 
-@app.route('/upload', methods=['POST'])
-def upload():
-    if 'file' not in request.files:
-        return render_template('index.html', error='No file part')
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-    file = request.files['file']
+@app.route('/process_frame', methods=['POST'])
+def process_frame():
+    frame_data = request.data  # Receive frame data from the client
 
-    if file.filename == '':
-        return render_template('index.html', error='No selected file')
+    # Your machine learning prediction code here
+    # Example: Replace this with your actual machine learning prediction logic
+    prediction = model.predict([np.frombuffer(frame_data, dtype=np.uint8)])
 
-    if file:
-        image = Image.open(io.BytesIO(file.read()))
-        result = process_gesture(np.array(image))
-        return render_template('result.html', result=result)
+    predicted_character = chr(ord('A') + int(prediction[0]))
 
+    return jsonify({'prediction': predicted_character})
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
