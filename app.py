d1aa852259d76e16dd70afa28d6174d78b4f8cf5
anwesa-sinha <sinhaanwesa@gmail.com>
2023-11-27 12:22:57 +0530

    changes in app.py

--- app.py
+++ app.py
@@ -2,15 +2,30 @@ from flask import Flask, render_template, Response, jsonify, request
 import cv2
 import numpy as np
 import pickle
+import mediapipe as mp
 
 app = Flask(__name__)
 
-model_dict = pickle.load(open('ML/model.pickle', 'rb'))
+model_dict = pickle.load(open('./ML/model.pickle', 'rb'))
 model = model_dict['model']
 
 cap = cv2.VideoCapture(0)
 
+mp_hands = mp.solutions.hands
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+
+hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
+
+labels_dict = {
+    0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E', 5: 'F', 6: 'G', 7: 'H',
+    8: 'I', 9: 'J', 10: 'K', 11: 'L', 12: 'M', 13: 'N', 14: 'O', 15: 'P',
+    16: 'Q', 17: 'R', 18: 'S', 19: 'T', 20: 'U', 21: 'V', 22: 'W', 23: 'X', 24: 'Y', 25: 'Z',
+}
+
 @app.route('/')
+def home():
+    return render_template('index.html')
 
 def generate_frames():
     while True:
@@ -18,13 +33,48 @@ def generate_frames():
         if not success:
             break
         else:
-            # Process the frame and send it to the client
-            ret, buffer = cv2.imencode('.jpg', frame)
+            data_aux = []
+            x_ = []
+            y_ = []
+
+            H, W, _ = frame.shape
+            frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+            results = hands.process(frame_rgb)
+            if results.multi_hand_landmarks:
+                for hand_landmarks in results.multi_hand_landmarks:
+                    mp_drawing.draw_landmarks(
+                        frame,
+                        hand_landmarks,
+                        mp_hands.HAND_CONNECTIONS,
+                        mp_drawing_styles.get_default_hand_landmarks_style(),
+                        mp_drawing_styles.get_default_hand_connections_style()
+                    )
+
+                for hand_landmarks in results.multi_hand_landmarks:
+                    for i in range(len(hand_landmarks.landmark)):
+                        x = hand_landmarks.landmark[i].x
+                        y = hand_landmarks.landmark[i].y
+                        data_aux.append(x)
+                        data_aux.append(y)
+                        x_.append(x)
+                        y_.append(y)
+
+                x1 = int(min(x_) * W) - 10
+                y1 = int(min(y_) * H) - 10
+                x2 = int(max(x_) * W) - 10
+                y2 = int(max(y_) * H) - 10
+
+                prediction = model.predict([np.asarray(data_aux)])
+                predicted_character = labels_dict[int(prediction[0])]
+
+                cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 0), 4)
+                cv2.putText(frame, predicted_character, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 0, 0), 3, cv2.LINE_AA)
+
+            _, buffer = cv2.imencode('.jpg', frame)
             frame_data = buffer.tobytes()
             yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame_data + b'\r\n')
-def home():
-    return render_template('index.html')
+                    b'Content-Type: image/jpeg\r\n\r\n' + frame_data + b'\r\n')
 
 @app.route('/video_feed')
 def video_feed():
@@ -32,14 +82,15 @@ def video_feed():
 
 @app.route('/process_frame', methods=['POST'])
 def process_frame():
-    frame_data = request.data  # Receive frame data from the client
+    frame_data = request.data
 
-    # Your machine learning prediction code here
-    # Example: Replace this with your actual machine learning prediction logic
-    prediction = model.predict([np.frombuffer(frame_data, dtype=np.uint8)])
+    # Your existing machine learning prediction code here
+    data_aux = []
+    for byte in frame_data:
+        data_aux.append(byte)
+    prediction = model.predict([np.asarray(data_aux)])
 
-    predicted_character = chr(ord('A') + int(prediction[0]))
-
+    predicted_character = labels_dict[int(prediction[0])]
     return jsonify({'prediction': predicted_character})
 
 if __name__ == "__main__":
